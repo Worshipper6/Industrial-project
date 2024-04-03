2dbcc2fcca30a87ea3ed7c56a40a1298c6519feb
du1125538821 <du1125538821@163.com>
2024-04-03 18:05:19 +0800

    AML main.py

--- FedML/python/examples/federate/prebuilt_jobs/fedgraphnn/AML/AML_FL.py
+++ FedML/python/examples/federate/prebuilt_jobs/fedgraphnn/AML/AML_FL.py
@@ -1,13 +1,14 @@
 import time
+import fedml
 import logging
-import os
-import sys
 from data_loading import get_data
-from training import train_gnn
+from train_util import extract_param
 from FL_loading import load_partition_data
-# from fedml import FedMLRunner
-# from trainer.fed_AML_trainer import FedSubgraphLPTrainer
-# from trainer.fed_AML_aggregator import FedSubgraphLPAggregator
+from fedml import FedMLRunner
+from models import RGCN, PNA
+
+from trainer.fed_AML_trainer import FedClsTrainer
+from trainer.fed_AML_aggregator import FedSubgraphLPAggregator
 
 # def logger_setup():
 #     # Setup logging
@@ -41,8 +42,21 @@ class Args():
         self.wandb = wandb
 
 
-args = Args(ports=False, tds=False, model='pna', data=None, reverse_mp=False, ego=False, emlps=False,\
-            n_epochs=100, batch_size=8192, num_neighs=[100,100], tqdm=True, wandb=False)
+#args = Args(ports=False, tds=False, model='pna', data=None, reverse_mp=False, ego=False, emlps=False,\
+            #n_epochs=100, batch_size=8192, num_neighs=[100,100], tqdm=True, wandb=False)
+
+def create_model(args, model_name):
+    logging.info("create_model. model_name = %s" % (model_name))
+    if model_name == "rgcn":
+        model = RGCN(
+            num_features=1, edge_dim=3, num_relations=8, num_gnn_layers=round(args.n_gnn_layers),
+            n_classes=2, n_hidden=round(args.n_hidden),
+            edge_update=args.emlps, dropout=args.dropout, final_dropout=args.final_dropout, n_bases=None  # (maybe)
+        )
+
+    else:
+        raise Exception("such model does not exist !")
+    return model
 
 
 #get data
@@ -78,22 +92,29 @@ def load_data(args):
     return dataset
 
 if __name__ == "__main__":
-    # # init FedML framework
-    # args = fedml.init()
-    #
-    # # init device
-    # device = fedml.device.get_device(args)
+    # init FedML framework
+    args = fedml.init()
+
+    # init device
+    device = fedml.device.get_device(args)
 
 
     # load data
     dataset = load_data(args)
 
-    # # load model
-    # model = create_model(args, args.model, dataset[7])
+    # load model
+    args.n_gnn_layers = extract_param("n_gnn_layers", args)
+    args.dropout = extract_param("dropout", args)
+    args.final_dropout = extract_param("final_dropout", args)
+    args.n_hidden = extract_param("n_hidden", args)
+    model = create_model(args, args.model)
+
+    # load trainer
+    args.w_ce1 = extract_param("w_ce1", args)
+    args.w_ce2 = extract_param("w_ce2", args)
+    trainer = FedClsTrainer(model, args)
+    aggregator = FedSubgraphLPAggregator(model, args)
     #
-    # trainer = FedSubgraphLPTrainer(model, args)
-    # aggregator = FedSubgraphLPAggregator(model, args)
-    #
-    # # start training
-    # fedml_runner = FedMLRunner(args, device, dataset, model, trainer, aggregator)
-    # fedml_runner.run()
+    # start training
+    fedml_runner = FedMLRunner(args, device, dataset, model, trainer, aggregator)
+    fedml_runner.run()
